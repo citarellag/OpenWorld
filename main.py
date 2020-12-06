8016b2d1bf5b0451b39c11f57f44a99bd70a7d52
Gaetandroid <citarellagaetano26@gmail.com>
2020-12-06 19:08:20 +0100

    6/12

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ RED = (255, 0, 0)
 display = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 gameMap = pytmx.load_pygame("Tiles\provona.tmx")
-FPS=300
+FPS=60
 spostamento_x=0
 spostamento_y=0
 
@@ -31,13 +31,13 @@ while True:
         if keys [pygame.K_ESCAPE]:
             quit()
         if keys[pygame.K_LEFT]: 
-                spostamento_x+=2
+                spostamento_x+=3
         if keys[pygame.K_RIGHT]:
-                spostamento_x-=2
+                spostamento_x-=3
         if keys[pygame.K_UP]:
-                spostamento_y+=2
+                spostamento_y+=3
         if keys[pygame.K_DOWN]:
-                spostamento_y-=2
+                spostamento_y-=3
 
     display.fill(BLACK)
     for layer in gameMap.visible_layers:
@@ -46,6 +46,6 @@ while True:
                 if(tile != None):
                     display.blit(tile, (x * gameMap.tilewidth+spostamento_x, y * gameMap.tileheight+spostamento_y))
     blit_button(288, 288, 24, 24, RED, "wood.jpg")
-    print ('tick={}, fps={}'.format(clock.tick(FPS), clock.get_fps()))
+    #print ('tick={}, fps={}'.format(clock.tick(FPS), clock.get_fps()))
 
     pygame.display.update()
