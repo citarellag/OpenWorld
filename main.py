61a82f3b1026a579e8a1ac0dadc79e84c5dd4d0a
Gaetandroid <citarellagaetano26@gmail.com>
2020-12-06 15:23:08 +0100

    6/12

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ pygame.init()
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-display = pygame.display.set_mode((600,400))
+display = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 gameMap = pytmx.load_pygame("Tiles\provona.tmx")
-FPS=60
+FPS=300
 spostamento_x=0
 spostamento_y=0
 
@@ -21,7 +21,7 @@ def blit_button(pos_x, pos_y, dim_x, dim_y, color, image_def):
 
 
 while True:
-    display = pygame.display.set_mode((600,400))
+    display = pygame.display.set_mode((600,600))
     clock.tick(FPS)
     
     for event in pygame.event.get():
@@ -31,13 +31,13 @@ while True:
         if keys [pygame.K_ESCAPE]:
             quit()
         if keys[pygame.K_LEFT]: 
-                spostamento_x+=10
+                spostamento_x+=2
         if keys[pygame.K_RIGHT]:
-                spostamento_x-=10
+                spostamento_x-=2
         if keys[pygame.K_UP]:
-                spostamento_y+=10
+                spostamento_y+=2
         if keys[pygame.K_DOWN]:
-                spostamento_y-=10
+                spostamento_y-=2
 
     display.fill(BLACK)
     for layer in gameMap.visible_layers:
@@ -45,7 +45,7 @@ while True:
                 tile = gameMap.get_tile_image_by_gid(gid)
                 if(tile != None):
                     display.blit(tile, (x * gameMap.tilewidth+spostamento_x, y * gameMap.tileheight+spostamento_y))
-    blit_button(288, 288, 32, 32, RED, "wood.jpg")
-    #print ('tick={}, fps={}'.format(clock.tick(FPS), clock.get_fps()))
+    blit_button(288, 288, 24, 24, RED, "wood.jpg")
+    print ('tick={}, fps={}'.format(clock.tick(FPS), clock.get_fps()))
 
     pygame.display.update()
