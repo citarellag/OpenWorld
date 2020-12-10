82a0d0d356c66a402424e0f5f37fb9c8b9d91475
Tond28 <filippotondelli@gmail.com>
2020-12-10 22:01:06 +0100

    Creazione hitbox blocco

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def blit_button(pos_x, pos_y, dim_x, dim_y, color, image_def):
     display.blit(image, button)
     return button
 
+controllo_up=0
+controllo_down=0
+controllo_dx=0
+controllo_sx=0
 
 while True:
     display = pygame.display.set_mode((600,400))
@@ -35,15 +39,19 @@ while True:
         if keys [pygame.K_ESCAPE]:
             quit()
         if keys[pygame.K_LEFT]: 
+            if controllo_sx==0:
                 spostamento_x+=var_spost
                 x_blocco-=var_spost
         if keys[pygame.K_RIGHT]:
+            if controllo_dx==0:
                 spostamento_x-=var_spost
                 x_blocco+=var_spost
         if keys[pygame.K_UP]:
+            if controllo_up==0:
                 spostamento_y+=var_spost
                 y_blocco-=var_spost
         if keys[pygame.K_DOWN]:
+            if controllo_down==0:
                 spostamento_y-=var_spost
                 y_blocco+=var_spost
     block = pygame.Rect(x_blocco, y_blocco, 24, 24)
@@ -59,11 +67,22 @@ while True:
                         display.blit(image, (x * gameMap.tilewidth+spostamento_x, y * gameMap.tileheight+spostamento_y))
         layer_index += 1
         if isinstance(layer, pytmx.TiledObjectGroup):
+            controllo_up=0
+            controllo_down=0
+            controllo_dx=0
+            controllo_sx=0
             if layer.name == "hit block":
                 for obj in layer:
                     if pygame.Rect(obj.x, obj.y, obj.width, obj.height).colliderect(block) == True:
-                        print("HITBOX!\nESCI!")
-                        break
+                        if obj.x>x_blocco:
+                            controllo_dx=1
+                        elif obj.x<x_blocco:
+                            controllo_sx=1
+                        if obj.y>y_blocco:
+                            controllo_down=1
+                        elif obj.y<y_blocco:
+                            controllo_up=1
+
     
     a=blit_button(288, 288, 24, 24, RED, "wood.jpg")
     #print ('tick={}, fps={}'.format(clock.tick(FPS), clock.get_fps()))
