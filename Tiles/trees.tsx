99e17611d4cf92af213ff24221995ae44f34ea2c
Gaetandroid <citarellagaetano26@gmail.com>
2021-01-07 11:05:02 +0100

    aggiunta hitbox 3

---
+++ Tiles/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="trees" tilewidth="32" tileheight="32" tilecount="48" columns="4">
+ <image source="../IMAGESOW/Trees.png" width="128" height="384"/>
+</tileset>
